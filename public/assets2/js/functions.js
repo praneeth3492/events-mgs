7f2433380dc3c7612e2875610bc820cc4188e3fe
Praneeth Kumar T <96046463+praneeth3492@users.noreply.github.com>
2023-12-07 14:03:01 +0530

    v1

---
+++ public/assets2/js/functions.js
@@ -0,0 +1,122 @@
+/*global jQuery */
+/* Contents
+// ------------------------------------------------>
+	1.  Background INSERT
+	2.  HEADER AFFIX
+	3.	AJAX MAILCHIMP
+	4.  AJAX CAMPAIGN MONITOR 
+	5.  OWL CAROUSEL
+	6.  SCROLL TO
+	7.  WOW
+	8.  Youtube Background
+*/
+(function($) {
+    "use strict";
+
+    /* ------------------  Background INSERT ------------------ */
+
+    var $bgSection = $(".bg-section");
+
+    $bgSection.each(function() {
+        var bgSrc = $(this).children("img").attr("src");
+        var bgUrl = 'url(' + bgSrc + ')';
+        $(this).parent().css("backgroundImage", bgUrl);
+        $(this).parent().addClass("bg-section");
+        $(this).remove();
+    });
+
+    /* ------------------ HEADER AFFIX ------------------ */
+ 
+
+     
+    /* ------------------ OWL CAROUSEL ------------------ */
+
+    $(".carousel").each(function() {
+        var $Carousel = $(this);
+        $Carousel.owlCarousel({
+            loop: $Carousel.data('loop'),
+            autoplay: $Carousel.data("autoplay"),
+            margin: $Carousel.data('space'),
+            nav: $Carousel.data('nav'),
+            dots: $Carousel.data('dots'),
+            dotsSpeed: $Carousel.data('speed'),
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: $Carousel.data('slide-res')
+                },
+                1000: {
+                    items: $Carousel.data('slide'),
+                }
+            }
+        });
+    });
+
+    /* ------------------  SCROLL TO ------------------ */
+
+    var aScroll = $('a[data-scroll="scrollTo"]');
+    aScroll.on('click', function(event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top-100
+            }, 1000);
+        }
+    });
+	
+	/* ------------------ NAV SPLIT ------------------ */
+	
+    var $section = $('.section'),
+        $bodyScroll = $('.body-scroll');
+    if ($bodyScroll.length > 0) {
+        $(window).on("scroll", function() {
+            $section.each(function() {
+                var sectionID = $(this).attr("id"),
+                    sectionTop = $(this).offset().top - 100,
+                    sectionHight = $(this).outerHeight(),
+                    wScroll = $(window).scrollTop(),
+                    $navHref = $("a[href='#" + sectionID + "']"),
+                    $nav = $('.nav-split').find($navHref).parent();
+                if (wScroll > sectionTop - 1 && wScroll < sectionTop + sectionHight - 1) {
+                    $nav.addClass('active');
+                    $nav.siblings().removeClass('active');
+                }
+            });
+        });
+    }
+
+    /* ------------------  WOW Animated ------------------ */
+    var wow = new WOW({
+
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 50, // distance to the element when triggering the animation (default is 0)
+        mobile: false, // trigger animations on mobile devices (default is true)
+        live: true // act on asynchronously loaded content (default is true)
+
+    });
+
+    wow.init();
+
+     
+	
+	 /* ------------------ MAGNIFIC POPUP ------------------ */
+
+    var $imgPopup = $(".img-popup");
+    $imgPopup.magnificPopup({
+        type: "image"
+    });
+    $('.img-gallery-item').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+	
+    
+
+
+}(jQuery));
